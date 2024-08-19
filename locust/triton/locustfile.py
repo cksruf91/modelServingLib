937e39e75dd8fed49e6395a7d829eb58ee384b52
박찬결 <chan10@hanatour.com>
2024-08-19 13:49:59 +0900

    [triton] locust update

--- locust/triton/locustfile.py
+++ locust/triton/locustfile.py
@@ -1,9 +1,9 @@
 from locust import HttpUser, task, between
 
 
-class EmbeddingUser(HttpUser):
+class LocustUser(HttpUser):
     wait_time = between(0.1, 0.4)
-    embedding_endpoint = "/v2/models/embedding/versions/1/infer"
+    embedding_endpoint = "/v2/models/cls/versions/1/infer"
     abstract = True
 
     @staticmethod
@@ -13,7 +13,7 @@ class EmbeddingUser(HttpUser):
         ]
         shape = [len(data), len(data[0])]
         body = {
-            "name": "embedding",
+            "name": "cls",
             "inputs": [
                 {
                     "name": "input_text",
@@ -26,26 +26,33 @@ class EmbeddingUser(HttpUser):
         return body
 
 
-class User1(EmbeddingUser):
-    @task(weight=1)
-    def embedding(self):
+class User1(LocustUser):
+
+    @task
+    def cls_request(self):
         text = "슈퍼 엔저 장기화에…한국 수출∙경상수지에 비상등"
         body = self._get_body(text)
-        self.client.post(self.embedding_endpoint, json=body)
+        with self.client.post(self.embedding_endpoint, json=body, catch_response=True) as response:
+            if response.elapsed.total_seconds() > 1.5:
+                response.failure("Request took too long")
 
 
-class User2(EmbeddingUser):
+class User2(LocustUser):
     @task(weight=1)
-    def embedding(self):
+    def cls_request(self):
         text = "Kamala Harris is more trusted than Donald Trump on the US economy"
         body = self._get_body(text)
-        self.client.post(self.embedding_endpoint, json=body)
+        with self.client.post(self.embedding_endpoint, json=body, catch_response=True) as response:
+            if response.elapsed.total_seconds() > 1.5:
+                response.failure("Request took too long")
 
 
-class User3(EmbeddingUser):
+class User3(LocustUser):
     @task(weight=1)
-    def embedding(self):
+    def cls_request(self):
         text = ("The fact that voters were more positive on Harris than on Biden... "
                 "says as much about how badly Biden was doing as it does about how well Harris is doing,")
         body = self._get_body(text)
-        self.client.post(self.embedding_endpoint, json=body)
+        with self.client.post(self.embedding_endpoint, json=body, catch_response=True) as response:
+            if response.elapsed.total_seconds() > 1.5:
+                response.failure("Request took too long")
